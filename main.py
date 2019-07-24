659be9f592f53ce9d5e3f294a7589380b5170c26
kfrn <knfrances@gmail.com>
2019-07-25 08:08:31 +1200

    Code style: naming
    Greater clarity; snake_case-style Pythonic naming

--- main.py
+++ main.py
@@ -10,10 +10,10 @@ from ffprobe3 import FFProbe
 # Includes ratios called in confirmation of FFprobe stream request.
 # Gives shortform name for each expected corresponding .png watermark file.
 # .png files are sized the same as SD, HD, Full HD and cropped HD (1440x1080) video
-aspects = [[720,  "4:3",  "SD"],
-          [1280, "16:9",  "HD"],
-          [1920, "16:9", "FHD"],
-          [1440,  "4:3", "CHD"]]
+RESOLUTIONS = [[720,  "4:3",  "SD"],
+              [1280, "16:9",  "HD"],
+              [1920, "16:9", "FHD"],
+              [1440,  "4:3", "CHD"]]
 
 # Main function, script entrypoint.
 def main():
@@ -25,109 +25,109 @@ def main():
         # If the number of arguments passed is greater than or equal to 2...
         if len(sys.argv) >= 2:
                 # Set the path variable to the 2nd argument (path).
-                path = sys.argv[1]
+                input_file = sys.argv[1]
 
                 # If the path is a legitimate file...
-                if (os.path.isfile(path)):
+                if (os.path.isfile(input_file)):
                         # Loop until break is called.
                         while True:
                                 # Obtain user input.
-                                var = input("Do you want to trim this file? (y/n)\n")
-                                if not (var == "y" or var == "n"):
+                                to_trim = input("Do you want to trim this file? (y/n)\n")
+                                if not (to_trim == "y" or to_trim == "n"):
                                         print("Invalid input, please enter either 'y' or 'n'.")
                                 else:
                                         break
                                 
                         # Get aspect ratio, resolution, and watermark image.
-                        aspectRatio, resolution = getARandRes(path)
-                        watermark = ffmpeg.input(getWatermark(aspectRatio))
+                        aspect_ratio, resolution = aspect_ratio_and_resolution(input_file)
+                        watermark = ffmpeg.input(watermark_path(aspect_ratio))
 
                         # Ask user for their preferred output filename/filepath.
-                        outpath = input("Where would you like the output saved?\n"
+                        output_path = input("Where would you like the output saved?\n"
                                         "e.g. 'home/Dave/Desktop/output.mp4'\n"
                                         "Providing only a filename will export to the directory the script is in.\n"
                                         "e.g. 'file.mp4' with no path.\n")
 
                         while True:
                                 # Obtain user input.
-                                wtrmrk = input("Do you want a watermark overlay on the output video? (y/n)")
-                                if not (wtrmrk == "y" or wtrmrk == "n"):
+                                watermark = input("Do you want a watermark overlay on the output video? (y/n)")
+                                if not (watermark == "y" or watermark == "n"):
                                         print("Invalid input, please enter either 'y' or 'n'.")
                                 else:
                                         break
 
                         # If the user selects to not trim...
-                        if var == "n":
-                                if wtrmrk == "y":
+                        if to_trim == "n":
+                                if watermark == "y":
                                         # Make the ffmpeg call.
                                         (
                                                 ffmpeg
-                                                .input(sys.argv[1])
-                                                .filter('setdar', getDAR(aspectRatio))
+                                                .input(input_file)
+                                                .filter('setdar', display_aspect_ratio(aspect_ratio))
                                                 .overlay(watermark)
-                                                .output(outpath)
+                                                .output(output_path)
                                                 .run()
                                         )
                                 else:
                                         # Make the ffmpeg call.
                                         (
                                                 ffmpeg
-                                                .input(sys.argv[1])
-                                                .filter('setdar', getDAR(aspectRatio))
-                                                .output(outpath, vcodec = "libx264", pix_fmt = "yuv420p", acodec = "aac", crf = "23")
+                                                .input(input_file)
+                                                .filter('setdar', display_aspect_ratio(aspect_ratio))
+                                                .output(output_path, vcodec = "libx264", pix_fmt = "yuv420p", acodec = "aac", crf = "23")
                                                 .run()
                                         )
 
                         # If the user selects to trim...
-                        elif var == "y":
+                        elif to_trim == "y":
                                 # Get the start and end trim in points from the user.
-                                trimstart = input("Please specify the trim 'in' point. (hh:mm:ss.mls 00:00:00.000)\n")
-                                trimend = input("Please specify the trim 'out' point. (hh:mm:ss.mls 00:00:00.000)\n")
-                                
-                                if wtrmrk == "y":
+                                in_point = input("Please specify the trim 'in' point. (hh:mm:ss.mls 00:00:00.000)\n")
+                                out_point = input("Please specify the trim 'out' point. (hh:mm:ss.mls 00:00:00.000)\n")
+
+                                if watermark == "y":
                                         # Make the ffmpeg call.
                                         (
                                                 ffmpeg
-                                                .input(sys.argv[1])
-                                                .trim(start = trimstart, end = trimend)
+                                                .input(input_file)
+                                                .trim(start = in_point, end = out_point)
                                                 .overlay(watermark)
-                                                .output(outpath, vcodec = "libx264", pix_fmt = "yuv420p", acodec = "aac", crf = "23")
+                                                .output(output_path, vcodec = "libx264", pix_fmt = "yuv420p", acodec = "aac", crf = "23")
                                                 .run()
                                         )
                                 else:
                                         # Make the ffmpeg call.
                                         (
                                                 ffmpeg
-                                                .input(sys.argv[1])
-                                                .trim(start = trimstart, end = trimend)
-                                                .output(outpath, vcodec = "libx264", pix_fmt = "yuv420p", acodec = "aac", crf = "23")
+                                                .input(input_file)
+                                                .trim(start = in_point, end = out_point)
+                                                .output(output_path, vcodec = "libx264", pix_fmt = "yuv420p", acodec = "aac", crf = "23")
                                                 .run()
                                         )
                 else:
                         print("ERROR: Path not valid.")
 
 # Function to parse the trim time from the given string.
-def parseTime(timeString):
+def parse_time(time_string):
         # Splits input time into format [hours, minutes, [seconds, milliseconds]].
-        return [val.split('.') for val in timeString.split(':')]
+        return [val.split('.') for val in time_string.split(':')]
 
 # Function to get the DAR from a given aspect ratio string.
-def getDAR(aspectRatio):
-        vals = [int(val) for val in aspectRatio.split(':')]
+def display_aspect_ratio(string):
+        vals = [int(val) for val in string.split(':')]
         return vals[0] / vals[1]
 
 # Function to get the aspect ratio and resolution from a given path to a video.
-def getARandRes(path):
-        inputfile = FFProbe(path)
+def aspect_ratio_and_resolution(file):
+        file_metadata = FFProbe(file)
 
-        for stream in inputfile.streams:
+        for stream in file_metadata.streams:
                 if stream.is_video():
                         width, height = stream.frame_size()
                         
                         ratio = ""
-                        for aspect in aspects:
-                                if aspect[0] == width:
-                                        ratio = aspect[1]
+                        for resolution in RESOLUTIONS:
+                                if resolution[0] == width:
+                                        ratio = resolution[1]
                                         break
 
                         if ratio == "":
@@ -137,14 +137,14 @@ def getARandRes(path):
         return ratio, [width, height]
 
 # Function to get a path to a corresponding .png watermark given an aspect ratio string.
-def getWatermark(ratio):
-        for aspect in aspects:
-                if aspect[1] == ratio:
-                        standard = aspect[2]
+def watermark_path(ratio):
+        for resolution in RESOLUTIONS:
+                if resolution[1] == ratio:
+                        standard = resolution[2]
 
-        pngname = "{0}.png".format(standard)
-        pngpath = os.path.join(os.getcwd(), "watermarks", pngname)
-        return pngpath
+        file_name = "{0}.png".format(standard)
+        file_path = os.path.join(os.getcwd(), "watermarks", file_name)
+        return file_path
 
 # Run the main function as entrypoint.
 if __name__ == '__main__':
